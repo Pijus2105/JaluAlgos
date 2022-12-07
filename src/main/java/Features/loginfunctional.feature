2ad0de9ce9e2be44ad73a07c2a8cbeeb40f33b66
Elphill <Elphill@DESKTOP-QEBQ8BA>
2022-12-07 18:22:07 +0530

    commit

--- src/main/java/Features/loginfunctional.feature
+++ src/main/java/Features/loginfunctional.feature
@@ -1,23 +1,70 @@
 Feature: Feature to test login functionality
 
-
-Scenario Outline: Login of scenario
+#*TC-02 & 03
+Background: Open the browser and verify login page title
 
 
 Given open the browser
 When verify login page title
 
+@tag1
+Scenario:  User cannot login with a blank credentials
+
+When verify if a user cannot login with a blank credentials
 
 
+
+Scenario Outline: User cannot login with a valid username and blank password
+
+When verify if a user cannot login with an invalid and valid "<username>" and blank password
+Then verify user able to see validation if user not enter of password "<Emailmessage>"
+
+#*TC-05 & 06
+Examples:
+     
+     | username | Emailmessage |
+     | admin1@gmail.com | password is not allowed to be empty |
+     | admin@gmail.com | password is not allowed to be empty |
+     
+
+#*TC-07 & 08
+Scenario Outline: User cannot login with a blank username and invalid password
+
+When Verify if a user cannot login with a blank username and invalid valid and valid "<password>"
 
 Examples:
+     
+     | password |
+     | test@123 | 
+     | test | 
+     | test1234 |
+     | 123456 |
+     
+ 
+ #*TC-09-013
+ 
+ Scenario Outline: User cannot login with a invalid credentials  
+ 
+ When verify if a user cannot login with a valid "<username>" and an invalid "<password>"
+ Then verify the "<invalidmessages>" for invalid login
+ 
+ Examples:
+ 
+     | username | password | invalidmessages |
+     | admin@gmail.com | 12345 | User not Found |
+     | admin1@gmail.com | 123456 | User not Found |
+     | admin1@gmail.com | 12345 | User not Found |
+     
+     
+Scenario Outline: User cannot login with a valid credentials 
+     
+Then Verify if a user will be able to login with a valid "<username>" and valid "<password>"
+ 
+Examples:
+     | username | password |
+     | admin@gmail.com | 123456 |       
+ 
 
-    | username | password |
-    | admin@gmail.com | ab123456 |
-    | admininvalid@gmail.com | 123456 |
-    | admininvalid@gmail.com | ab123456 |
-    | admin@gmail.com | 123456 |
-    
-
-
+ 
+     
     
--- src/main/java/StepDefinition/LoginSteps.java
+++ src/main/java/StepDefinition/LoginSteps.java
@@ -6,6 +6,7 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -45,38 +46,96 @@ public class LoginSteps {
 			System.out.println("Actual_Title_Of_Page : - " + Actual_Title_Of_Page );
 
 		}
-
-	       
-	  
-
+ 	
 	
-	  
-	  @Then("^verify if a user will be able to login with a \"([^\"]*)\" and a \"([^\"]*)\"$")
-	    public void verify_if_a_user_will_be_able_to_login_with_a_something_and_a_something(String username, String password) throws Throwable {
-		     WebElement Email = driver.findElement(By.xpath("//input[@type='email']"));
-		     
+
+	 @When("^verify if a user cannot login with a blank credentials$")
+	    public void verify_if_a_user_cannot_login_with_a_blank_credentials() throws Throwable {
+		 WebElement click = driver.findElement(By.xpath("//button[@class='btn sign_in_btn']"));
+	     click.click();
+	
+	     System.out.println("Please filled Username And Password");
+	    }
+
+	    	    
+	 
+	    
+	    @When("^verify if a user cannot login with an invalid and valid \"([^\"]*)\" and blank password$")
+	    public void verify_if_a_user_cannot_login_with_an_invalid_and_valid_something_and_blank_password(String username) throws Throwable {
+	    	 WebElement Email = driver.findElement(By.xpath("//input[@type='email']"));
 			 Email.sendKeys(username);
+			 
+			 WebElement click = driver.findElement(By.xpath("//button[@class='btn sign_in_btn']"));
+		     click.click();
+	    }
+
+	    @Then("^verify user able to see validation if user not enter of password \"([^\"]*)\"$")
+	    public void verify_user_able_to_see_validation_if_user_not_enter_of_password_something(String emailmessage) throws Throwable {
+	    	WebElement message_validation = driver.findElement(By.xpath("//div[@role='alert']"));
+	    	String received_message = message_validation.getText();
+	    	
+	    	if(emailmessage == received_message) {
+	    		System.out.println("Error Message Is : - " + received_message);
+	    	} else {
+	    		System.out.println("Actual Message Is : - " + received_message);
+	    		System.out.println("Expected Message Is : - " + emailmessage);
+	    	}
+	    	
+	    	
+	    }
+	    
+	  
+	    @When("^Verify if a user cannot login with a blank username and invalid valid and valid \"([^\"]*)\"$")
+	    public void verify_if_a_user_cannot_login_with_a_blank_username_and_invalid_valid_and_valid_something(String password) throws Throwable {
+	    	 WebElement pwd = driver.findElement(By.xpath("//input[@type='password']"));
+			 pwd.sendKeys(password);
+			 pwd.sendKeys(Keys.ENTER);
+		
+			 System.out.println("Email Id Is Required");
+	    }
+	    
+	    @When("^verify if a user cannot login with a valid \"([^\"]*)\" and an invalid \"([^\"]*)\"$")
+	    public void verify_if_a_user_cannot_login_with_a_valid_something_and_an_invalid_something(String username, String password) throws Throwable {
+	    	 WebElement Email = driver.findElement(By.xpath("//input[@type='email']"));
+			 Email.sendKeys(username);
+			 
 			 WebElement pwd = driver.findElement(By.xpath("//input[@type='password']"));
 			 pwd.sendKeys(password);
+			 
+			 WebElement click = driver.findElement(By.xpath("//button[@class='btn sign_in_btn']"));
+		     click.click();
+			 			 
+	    }
+ 
+    	
+	    
+	    @Then("^verify the \"([^\"]*)\" for invalid login$")
+	    public void verify_the_something_for_invalid_login(String invalidmessages) throws Throwable {
+	    	WebElement message_validation = driver.findElement(By.xpath("//div[@role='alert']"));
+	    	String received_message = message_validation.getText();
+	    	
+	    	if(invalidmessages == received_message) {
+	    		System.out.println("Error Message Is : - " + received_message);
+	    	} else {
+	    		System.out.println("Actual Message Is : - " + received_message);
+	    		System.out.println("Expected Message Is : - " + invalidmessages);
+	    	}
+	    	
+	    }
+	    
+	    
+	    @Then("^Verify if a user will be able to login with a valid \"([^\"]*)\" and valid \"([^\"]*)\"$")
+	    public void verify_if_a_user_will_be_able_to_login_with_a_valid_something_and_valid_something(String username, String password) throws Throwable {
+	    	WebElement Email = driver.findElement(By.xpath("//input[@type='email']"));
+			 Email.sendKeys(username);
+			 
+			 WebElement pwd = driver.findElement(By.xpath("//input[@type='password']"));
+			 pwd.sendKeys(password);
+			 
 			 WebElement click = driver.findElement(By.xpath("//button[@class='btn sign_in_btn']"));
 		     click.click();
 		     
+		     System.out.println("Admin On The Dashboard");
 	    }
-	  
-//	  @Then("^verify if a user is able to login with a new password only after he/she has  the \"([^\"]*)\"$")
-//	    public void verify_if_a_user_is_able_to_login_with_a_new_password_only_after_heshe_has_the_something(String strArg1) throws Throwable {
-//	        driver.findElement(By.xpath("//p[@class='avatar rounded-circle']")).click();
-//	        
-//	    }
-
-	 
-
-	  @Then("^verify if a user is able to login with a new password only after he she has  the changedpassword$")
-	    public void verify_if_a_user_is_able_to_login_with_a_new_password_only_after_he_she_has_the_changedpassword() throws Throwable {
-		  driver.findElement(By.xpath("//p[@class='avatar rounded-circle']")).click();
-	    }
-
-
-	
 
 }
